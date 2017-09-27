8afeb4e90e8c3e1fbd9ac78d3b071486a1c338c5
Andrei Mungiu <andrei_mungiu@yahoo.com>
2017-09-27 23:09:35 +0200

    Add project files.

---
+++ PersonRepository.Interface/IPersonRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace PersonRepository.Interface
+{
+    public interface IPersonRepository
+    {
+        //Interfaces can also be used as return type
+        IEnumerable<Person> GetPeople();
+
+        Person GetPerson(string lastName);
+
+        void AddPerson(Person newPerson);
+
+        void UpdatePerson(string lastName, Person updatedPerson);
+
+        void DeletePerson(string lastName);
+
+        void UpdatePeople(IEnumerable<Person> updatedPeople);
+    }
+}
